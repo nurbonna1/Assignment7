98c5fa4c82be778080dec6bde52462fd891665af
nurbonna1 <nurashekyn.bonna@student.adelaide.edu.au>
2025-06-03 21:09:00 +0930

    Add files via upload

---
+++ ParseTree.py
@@ -0,0 +1,16 @@
+class ParseTree:
+    def __init__(self, node_type):
+        self.node_type = node_type
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def __repr__(self, level=0):
+        ret = "  " * level + f"{self.node_type}\n"
+        for child in self.children:
+            if isinstance(child, ParseTree):
+                ret += child.__repr__(level + 1)
+            else:
+                ret += "  " * (level + 1) + repr(child) + "\n"
+        return ret
