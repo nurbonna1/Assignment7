3f0ab527426bcc55106b73d470c50fe89eee0774
nurbonna1 <nurashekyn.bonna@student.adelaide.edu.au>
2025-06-03 21:34:52 +0930

    Update ParseTree.py

--- ParseTree.py
+++ ParseTree.py
@@ -1,20 +1,13 @@
 class ParseTree:
     def __init__(self, node_type):
-        self.type = node_type
-        self.value = node_type
+        self.node_type = node_type
         self.children = []
 
     def addChild(self, child):
         self.children.append(child)
 
-    def getType(self):
-        return self.type
-
-    def getValue(self):
-        return self.value
-
     def __repr__(self, level=0):
-        ret = "  " * level + f"{self.value}\n"
+        ret = "  " * level + f"{self.node_type}\n"
         for child in self.children:
             if isinstance(child, ParseTree):
                 ret += child.__repr__(level + 1)
